a383399ba458e5c78751791106389a696f4a24ae
Kichkinev Vladislav <100088832+KIchkinevVladislav@users.noreply.github.com>
2022-07-27 00:01:28 +0300

    Update views.py

--- api/views.py
+++ api/views.py
@@ -8,10 +8,20 @@ from .serializers import PostSerializer, CommentSerializer, FollowSerializer, Gr
 from .permissions import OwnResoursePermission
 
 class PerformCreateMixsin:
+    """
+    Overriding perform_create function to save the requesting user as the author.
+    """
     def perform_create(self, serializer):
         return serializer.save(author=self.request.user)
 
 class PostViewSet(viewsets.ModelViewSet, PerformCreateMixsin):
+    """
+    Implemented:
+    - post creation
+    - editing publications
+    - get all posts
+    - get a separate post
+    """
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [OwnResoursePermission, permissions.IsAuthenticatedOrReadOnly]
@@ -20,6 +30,12 @@ class PostViewSet(viewsets.ModelViewSet, PerformCreateMixsin):
     filterset_fields = ['group',]
 
 class CommentViewSet(viewsets.ModelViewSet, PerformCreateMixsin):
+    """
+    Implemented:
+    - get all comments of a post
+    - create a new comment
+    - editing publications
+    """
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permissions = [OwnResoursePermission, permissions.IsAuthenticatedOrReadOnly]
@@ -30,6 +46,11 @@ class CommentViewSet(viewsets.ModelViewSet, PerformCreateMixsin):
         return Response(serializer.data)
 
 class FollowingViewSet(viewsets.ModelViewSet):
+    """
+    Implemented:
+    - getting a list of all subscribers
+    - create a subscription
+    """
     queryset = Follow.objects.all()
     serializer_class = FollowSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
@@ -38,6 +59,11 @@ class FollowingViewSet(viewsets.ModelViewSet):
     search_fields = ['=user__username', '=following__username']
 
 class GroupViewSet(viewsets.ModelViewSet, PerformCreateMixsin):
+    """
+    Implemented:
+    - getting a list of all groups
+    - to create a group
+    """
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
