bea59bb7405dc8e02224ecba1bfa446d348552ca
Kichkinev Vladislav <100088832+KIchkinevVladislav@users.noreply.github.com>
2023-02-09 20:22:41 +0300

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,7 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+
 from . views import  PostViewSet, CommentViewSet, FollowingViewSet, GroupViewSet
 
 router = DefaultRouter()
