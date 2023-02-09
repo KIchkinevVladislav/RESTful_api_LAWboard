3e661d948128c2ed83ff42ac403378db3a4dec27
Kichkinev Vladislav <100088832+KIchkinevVladislav@users.noreply.github.com>
2023-02-09 20:06:25 +0300

    Update urls.py

--- LAWboard_api/urls.py
+++ LAWboard_api/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
