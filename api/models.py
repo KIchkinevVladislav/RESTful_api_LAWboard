cbaa4206ea2919da7e5d81150e6aaef4877a2c95
Kichkinev Vladislav <100088832+KIchkinevVladislav@users.noreply.github.com>
2023-05-15 22:54:44 +0300

    Update models.py

--- api/models.py
+++ api/models.py
@@ -34,3 +34,5 @@ class Comment(models.Model):
 class Follow(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follower')
     following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+
+    
